2df3b7b07ac177b9275a783bb304ff20893dfb91
markhaynes456 <markhaynes456@gmail.com>
2015-05-09 06:54:35 -0400

    Added Paperclip and active place support

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150504194038) do
+ActiveRecord::Schema.define(version: 20150506204750) do
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
@@ -32,6 +32,10 @@ ActiveRecord::Schema.define(version: 20150504194038) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
     t.boolean  "active"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
