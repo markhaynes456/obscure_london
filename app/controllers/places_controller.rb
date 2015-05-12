bd98ad4c07d6edfbed206475e6fa18fa7b345f01
markhaynes456 <markhaynes456@gmail.com>
2015-05-12 17:35:07 -0400

    Revert "Added Active support (Buggy) Checkboxes"
    This reverts commit 32bd659419fbfeac3fee08b9f098f4c4fd06dd4e.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -19,21 +19,13 @@ class PlacesController < ApplicationController
 			flash[:notice] = "Error creating place"
 			render 'new' 
 		end
-	end
 
-	def update
-		@place = Place.find(params[:id])
-		if @place.update_attributes(place_params)
-			redirect_to(:sessions)
-		else
-			redirect_to(:sessions)
-		end
 	end
 
 	private
 	
 	def place_params
-		params.require(:place).permit(:id,:active,:name,:address_line_one,:address_line_two,:postcode,:tube_station,:price,:description,:opening_times_monday,:opening_times_tuesday,:opening_times_wednesday, :opening_times_thursday, :opening_times_friday, :opening_times_saturday,:category)
+		params.require(:place).permit(:name,:address_line_one,:address_line_two,:postcode,:tube_station,:price,:description,:opening_times_monday,:opening_times_tuesday,:opening_times_wednesday, :opening_times_thursday, :opening_times_friday, :opening_times_saturday,:category)
 	end
 
 end
