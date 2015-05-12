d4f800da522f8cf0332dc6f1758ef5af70395375
markhaynes456 <markhaynes456@gmail.com>
2015-05-12 16:57:51 -0400

    Revert "Troubleshooted Paperclip"
    This reverts commit 565cea88351672d93f130ce84adc9d4785536ac1.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511194403) do
+ActiveRecord::Schema.define(version: 20150506204750) do
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
