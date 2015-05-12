53512de7132b83c428bb0c3f0e1c0b4bb60c8003
markhaynes456 <markhaynes456@gmail.com>
2015-05-12 17:43:28 -0400

    Revert "Completed update tests and created new view"
    This reverts commit c62668b088def6b664b2ffbc802279c12b249e6d.

--- spec/controllers/places_controller_spec.rb
+++ spec/controllers/places_controller_spec.rb
@@ -21,23 +21,21 @@ RSpec.describe PlacesController, type: :controller do
 		describe("Place #create") do
 
 			it "Should redirect to index with a notice of succesful save" do
-				allow_any_instance_of(Place).to(receive(:valid?)).and_return(true)
-				post :create, :place => {:name => "name"}
-				expect(flash[:notice]).not_to(be_nil)
+				allow_any_instance_of(Place).to receive(:valid?).and_return(true)
+				@place = Place.new
+				post 'create' 
+				expect(assigns(flash[:notice])).to(not_be(nill))
+				expect(Place.find(1)).to(not_be(nil))
 				expect(response).to(redirect_to(places_url))
-				expect(Place.find(1)).not_to(be_nil)	
-		end
+			end
 
 			it "Should render new template with notice error on failed save" do
-				allow_any_instance_of(Place).to(receive(:valid?)).and_return(false)
-				post :create, :place => {:name => "name"}
-				expect(flash[:notice]).not_to(be_nil)
-				expect(response).to(render_template("new"))		
+				pending("To be implemented")
 			end
 
 			it "Should pass params to the create method" do
 				post :create, :place => {:address_line_one => "8 marston road", :address_line_two => "wicked aston", :postcode => "EH28 9QB", :name => "le Restaurante", :description => "lorem itsum postus iloem rem itusm epson loren istem epsun", :category => "Shopping"}
-				expect(assigns[:place].name).to( eq('le Restaurante') )
+				expect(assigns[:place].name).to( eq('place') )
 			end
 		
 		end
