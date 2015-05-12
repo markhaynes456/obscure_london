6d5078b2ce4277341be9c7d7333f896ffabc5c47
markhaynes456 <markhaynes456@gmail.com>
2015-05-12 17:43:02 -0400

    made small changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150416204345) do
+ActiveRecord::Schema.define(version: 20150501074108) do
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20150416204345) do
     t.string   "category"
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
