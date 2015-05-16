5dc0f62a864ca3886bd0f786d5f69f4a558d8b4a
markhaynes456 <markhaynes456@gmail.com>
2015-05-16 06:34:35 -0400

    User created with tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514200250) do
+ActiveRecord::Schema.define(version: 20150516102331) do
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 20150514200250) do
     t.string   "friday"
     t.string   "saturday"
     t.string   "sunday"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+	describe("Creating Users") do
+
+		it("Should create a user") do
+			@user = User.new(:name=>"Mark",:password=>"password")
+			expect(@user.valid?).to(be(true))
+		end
+
+		it("Should not create a user") do
+			@user = User.new(:name=>"Mark")
+			expect(@user.valid?).to(be(false))
+		end
+
+	end
   
 end
